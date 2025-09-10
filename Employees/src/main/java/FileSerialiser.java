eba297ec67be74aab2815450379bce8ac9364c87
Oliver Felgate <ofelgate@spartaglobal.com>
2025-09-10 16:53:06 +0100

    add jackson to dependencies and add list to json file method

--- Employees/src/main/java/FileSerialiser.java
+++ Employees/src/main/java/FileSerialiser.java
@@ -1,2 +1,27 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileSerialiser {
+
+    public static void employeeListToJsonFile(List<Employee> employees, String path) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<String> jsonList = new ArrayList<>();
+
+        for (Employee emp : employees) {
+            String empJson = mapper.writeValueAsString(emp);
+            jsonList.add(empJson);
+        }
+
+        try {
+            Files.write(Paths.get(path), jsonList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
