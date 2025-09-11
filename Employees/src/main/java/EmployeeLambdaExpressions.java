13bc5807db1a60b531851527220e28632f3c7899
Sajni Shah <sshah@spartaglobal.com>
2025-09-11 12:19:26 +0100

    task 5

--- Employees/src/main/java/EmployeeLambdaExpressions.java
+++ Employees/src/main/java/EmployeeLambdaExpressions.java
@@ -1,6 +1,5 @@
 import java.io.CharConversionException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class EmployeeLambdaExpressions {
 
@@ -11,14 +10,14 @@ public class EmployeeLambdaExpressions {
         EmployeeCsvReader reader = new EmployeeCsvReader();
         ArrayList<Employee> employees = reader.readEmployees(fileName);
 
-//        // -------- GENDER --------
-//
-//        //Remove all employees with gender 'M'
-//        employees.removeIf(emp -> emp != null && emp.getGender() == 'M');
-//
-//        //Print remaining employees
-//        System.out.println("Employees after removing males:");
-//        employees.forEach(System.out::println);
+        // -------- GENDER --------
+
+        //Remove all employees with gender 'M'
+        employees.removeIf(emp -> emp != null && emp.getGender() == 'M');
+
+        //Print remaining employees
+        System.out.println("Employees after removing males:");
+        employees.forEach(System.out::println);
 
         // -------- SALARY --------
 
@@ -26,6 +25,19 @@ public class EmployeeLambdaExpressions {
         employees.removeIf(emp -> emp != null && emp.getSalary() < 100000);
 
         System.out.println("Employees after removing salaries under 100,000:");
+        employees.forEach(System.out::println);
+
+        // -------- EMAILS --------
+
+        //Remove all employees with gmail, yahoo, or hotmail emails
+        employees.removeIf(emp ->
+                emp != null &&
+                        (emp.getEmail().toLowerCase().contains("gmail.com")
+                                || emp.getEmail().toLowerCase().contains("yahoo.co.uk")
+                                || emp.getEmail().toLowerCase().contains("hotmail.com"))
+        );
+
+        System.out.println("Employees after removing Gmail, Yahoo, and Hotmail users:");
         employees.forEach(System.out::println);
     }
 }
